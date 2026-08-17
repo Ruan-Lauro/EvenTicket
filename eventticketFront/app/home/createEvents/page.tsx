18a71946065dff576b9d83a45a43d08e511dfbb8
Ruan Lauro <ruanlauromonteiro@gmail.com>
2026-08-17 15:25:26 -0300

    hotfix: userId fixed in createEvents

--- eventticketFront/app/home/createEvents/page.tsx
+++ eventticketFront/app/home/createEvents/page.tsx
@@ -158,7 +158,7 @@ function CreateEvents() {
         <EventDetailModal
           event={selectedEvent}
           onClose={() => setSelectedEvent(null)}
-          userId={user?.id} 
+          userId={Number(user?.id)} 
         />
       )}
 
